48130ab9b48c60165155af222154f0b3675eab22
joel_garcia <jgarcia211@ucol.mx>
2024-11-11 14:51:14 -0600

    Se realiza el diseño UI de las cards y variables para recibir parametros

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CustomHeaderComponent } from '../custom-header/custom-header.component';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +7,11 @@ import { CustomHeaderComponent } from '../custom-header/custom-header.component'
 })
 export class HomePage {
 
+  // Variables para almacenar los valores que se recibiran de los sensores
+  lastReading: string = " "; // Ultima lectura
+  Temperature: string = " "; // Temperatura en grados
+  waterlevel: string = " "; // Nivel del agua como texto
+  ambientHumidity: string = " "; // Humedad del ambiente en porcentaje
+  soilHumidity: string = " "; // Humedad del suelo en porcentaje 
   constructor() { }
-
 }
