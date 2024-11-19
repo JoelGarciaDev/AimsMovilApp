677a2e2efe9d188741d80b7a614c03992a225887
WhiteBryz <davidflores@live.com.mx>
2024-11-18 20:55:01 -0600

    Downgrade versions of angular/fire y firebase and installed the new ones forcing them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { provideFirebaseApp, getApp } from '@angular/fire/app';
+import { provideDatabase } from '@angular/fire/database';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
   ],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideDatabase(() => getDatabase()),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAmsRjHA7JDSSvXK39fc7h52AAwgeKpAVY",
+    authDomain: "ucol-iot.firebaseapp.com",
+    databaseURL: "https://ucol-iot-default-rtdb.firebaseio.com",
+    projectId: "ucol-iot",
+    storageBucket: "ucol-iot.firebasestorage.app",
+    messagingSenderId: "395013315537",
+    appId: "1:395013315537:web:8df7396592764d999bdef0",
+    measurementId: "G-W1D0TWBZPJ"
+  }
 };
 
 /*
